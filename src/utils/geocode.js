cb5505fb9c0e80aa62ae8c53b560a8495b1c4b55
Hamidreza Ghavidel <h.ghavidel@phinix.com>
2021-11-23 19:01:26 +0330

    fix bugs

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,9 +4,9 @@ const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiaGFtaWRyZXphZ2hhdmlkZWwiLCJhIjoiY2t2dG8ydm1xYmU5djJ1czd0cDR6NHZ0biJ9.wp33cT5_47tvAIwVYpPw-Q'
     request({ url: url, json: true }, (error, {body}) => {
         if (error) {
-            callback('Unable to connect to location services!', undefined)
-        } else if (body.features === 0) {
-            callback('Unable to find location. Try another search.', undefined)
+            callback('Unable to connect to location services!', {})
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', {})
         } else {
             callback(undefined, {
                 latitude: body.features[0].center[0],
